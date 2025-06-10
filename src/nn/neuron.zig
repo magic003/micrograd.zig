9b0ad0709eda84612b7bda883093c9b14496c87b
Minjie Zha <diego003@gmail.com>
2025-06-09 22:26:15 -0700

    add parameters and zeroGrad to Neuron

--- src/nn/neuron.zig
+++ src/nn/neuron.zig
@@ -7,35 +7,49 @@ const Value = @import("../value.zig").Value;
 /// Neuron represents a single neuron in a neural network.
 pub const Neuron = struct {
     w: []Value(f32),
-    b: Value(f32),
+    b: *Value(f32),
     non_linear: bool = true,
     products: []Value(f32), // Temporary storage for products during forward pass
     sums: []Value(f32), // Temporary storage for sums during forward pass
     z: Value(f32), // Temporary storage for the final output during forward pass
+    parameters: []*Value(f32),
     allocator: Allocator,
 
     /// Creates a neuron given the input values.
     pub fn init(allocator: Allocator, num_input: usize, non_linear: bool) Allocator.Error!Neuron {
         const w = try allocator.alloc(Value(f32), num_input);
+        const b = blk: {
+            const bp = try allocator.create(Value(f32));
+            bp.* = Value(f32).init(0.0);
+            break :blk bp;
+        };
+        const parameters = try allocator.alloc(*Value(f32), num_input + 1); // +1 for bias
+
         var prng = Prng.init(@intCast(std.time.milliTimestamp()));
-        for (w) |*weight| {
+        for (w, 0..) |*weight, i| {
             weight.* = Value(f32).init(prng.random().float(f32) * 2.0 - 1.0); // [-1.0, 1.0)
+            parameters[i] = weight;
         }
+        parameters[parameters.len - 1] = b; // Last parameter is the bias
+
         return Neuron{
             .w = w,
-            .b = Value(f32).init(0.0),
+            .b = b,
             .non_linear = non_linear,
             .products = try allocator.alloc(Value(f32), num_input),
             .sums = try allocator.alloc(Value(f32), num_input),
             .z = Value(f32).init(0.0),
+            .parameters = parameters,
             .allocator = allocator,
         };
     }
 
     /// Deinitializes the neuron and frees its resources.
     pub fn deinit(self: Neuron) void {
+        self.allocator.free(self.parameters);
         self.allocator.free(self.sums);
         self.allocator.free(self.products);
+        self.allocator.destroy(self.b);
         self.allocator.free(self.w);
     }
 
@@ -50,8 +64,14 @@ pub const Neuron = struct {
             self.sums[i] = self.sums[i - 1].add(p);
         }
         // z = sum + b
-        self.z = self.sums[self.sums.len - 1].add(&self.b);
+        self.z = self.sums[self.sums.len - 1].add(self.b);
         return if (self.non_linear) self.z.relu() else self.z;
+    }
+
+    pub fn zeroGrad(self: *Neuron) void {
+        for (self.parameters) |p| {
+            p.grad = 0.0;
+        }
     }
 
     const testing = @import("std").testing;
@@ -68,6 +88,14 @@ pub const Neuron = struct {
             try testing.expectEqual(3, neuron.w.len);
             try testing.expectEqual(0.0, neuron.b.data);
             try testing.expectEqual(true, neuron.non_linear);
+            try testing.expectEqual(3, neuron.products.len);
+            try testing.expectEqual(3, neuron.sums.len);
+
+            try testing.expectEqual(4, neuron.parameters.len); // 3 weights + 1 bias
+            try testing.expectEqual(&neuron.w[0], neuron.parameters[0]);
+            try testing.expectEqual(&neuron.w[1], neuron.parameters[1]);
+            try testing.expectEqual(&neuron.w[2], neuron.parameters[2]);
+            try testing.expectEqual(neuron.b, neuron.parameters[3]);
         }
     }
 
